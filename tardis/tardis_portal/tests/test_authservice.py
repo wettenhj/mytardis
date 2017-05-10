de101ee956752efa6c905d3cee4e7460707e9369
James Wettenhall <James.Wettenhall@monash.edu>
2017-05-10 21:14:42 +1000

    Replacing deprecated assertion methods

--- tardis/tardis_portal/tests/test_authservice.py
+++ tardis/tardis_portal/tests/test_authservice.py
@@ -119,7 +119,7 @@ class AuthServiceTestCase(TestCase):
         c = Client()
         login = c.login(username='mockdb_user1', password='secret')
         self.assertTrue(login)
-        self.assert_(SESSION_KEY in c.session)
+        self.assertIn(SESSION_KEY, c.session)
 
         r = str(c.get('/test/groups/'))
         self.assertEqual(r.count('mockdb'), 2)
@@ -128,7 +128,7 @@ class AuthServiceTestCase(TestCase):
 
         login = c.login(username='mockdb_user2', password='secret')
         self.assertTrue(login)
-        self.assert_(SESSION_KEY in c.session)
+        self.assertIn(SESSION_KEY, c.session)
 
         r = str(c.get('/test/groups/'))
         self.assertEqual(r.count('mockdb'), 2, r)
